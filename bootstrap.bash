69e0055821fc77a3bef58317fdbb549fb3456bb8
Albert Chen <albertchen@berkeley.edu>
2022-06-07 13:07:20 -0700

    force versions in bootstrap.bash

--- bootstrap.bash
+++ bootstrap.bash
@@ -6,16 +6,16 @@ VERSIONS=$SCRIPT_DIR/versions.json
 
 SCALA3_VERSION="$(jq -r '.["scala3"]' $VERSIONS)"
 CATS_EFFECT_VERSION="$(jq -r '.["cats-effect"]' $VERSIONS)"
-mkdir -p build
-pushd build
+mkdir -p $SCRIPT_DIR/build
+pushd $SCRIPT_DIR/build
 
 git clone --depth 1 --branch v${CATS_EFFECT_VERSION} https://github.com/typelevel/cats-effect.git
 pushd cats-effect
-sbt ++${SCALA3_VERSION} kernelJVM/publishLocal
+sbt "project kernelJVM" "set version := \"${CATS_EFFECT_VERSION}\"" "++${SCALA3_VERSION} publishLocal"
 popd
 
 CATS_CORE_VERSION="$(jq -r '.["cats-core"]' $VERSIONS)"
 git clone --depth 1 --branch v${CATS_CORE_VERSION} https://github.com/typelevel/cats.git
 pushd cats
-sbt ++${SCALA3_VERSION} coreJVM/publishLocal
+sbt "project coreJVM" "set version := \"${CATS_CORE_VERSION}\"" "++${SCALA3_VERSION} publishLocal"
 popd
